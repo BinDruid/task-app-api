66528299dc7376d2f297ac1fe859b7e7c4979a4b
abharya <abharya.dev@gmail.com>
2023-10-22 09:50:54 +0330

    update: add simple celery task

---
+++ apps/core/tasks.py
@@ -0,0 +1,10 @@
+from time import sleep
+from celery import shared_task
+
+
+@shared_task()
+def email_task():
+    print(f"sending a temp email")
+    sleep(5)
+    print(f"email sent")
+    return "good"
---
+++ config/celery.py
@@ -0,0 +1,16 @@
+import os
+from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.development")
+if os.getenv("DJANGO_SETTINGS_MODULE"):
+    os.environ["DJANGO_SETTINGS_MODULE"] = os.getenv("DJANGO_SETTINGS_MODULE")
+
+app = Celery("config")
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.timezone = 'UTC'
