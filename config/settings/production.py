9e1c979e6e392c91961a71b3381b4574636b254d
abharya <abharya.dev@gmail.com>
2023-10-22 09:52:55 +0330

    update: add pytest and test modules

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,6 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["task.bindruid.ir"]
+ALLOWED_HOSTS = ["tasksapi.bindruid.ir"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "public/")
