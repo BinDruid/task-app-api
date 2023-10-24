000c5f716c8cc85e2aa9d7613f8fdcf55977906f
abharya <abharya.dev@gmail.com>
2023-10-24 12:42:30 +0330

    refactor: use payload

--- apps/tasks/tests/test_tasks.py
+++ apps/tasks/tests/test_tasks.py
@@ -25,13 +25,13 @@ class TestTaskCreation(TestCase):
         url = self.base_api_url + "/tasks/recent/"
         response = self.client.get(url)
         today = date.today().strftime("%d %b, %Y")
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data[today]), self.today_tasks_count)
 
     def test_user_can_create_task(self):
         url = self.base_api_url + "/tasks/"
-        response = self.client.post(url, data={"title": "new_task", "description": "test task has been created"})
+        payload = {"title": "new_test_task", "description": "test task to create"}
+        response = self.client.post(url, data=payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data["title"], "new_task")
+        self.assertEqual(response.data["title"], payload["title"])
