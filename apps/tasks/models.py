1bd0ae3efe38c98b398a63a98276dc29dd64072f
abharya <abharya.dev@gmail.com>
2023-10-24 12:37:39 +0330

    refactor: drop django jalali dependency

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,8 +1,7 @@
+import datetime
 from uuid import uuid4
 from django.db import models
 from django.contrib.auth import get_user_model
-from django_jalali.db import models as jmodels
-
 
 UserModel = get_user_model()
 
@@ -12,17 +11,13 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     owner = models.ForeignKey(UserModel, on_delete=models.PROTECT)
-    created_at = jmodels.jDateTimeField(auto_now_add=True, verbose_name="تاریخ ایجاد")
-    updated_at = jmodels.jDateTimeField(auto_now=True, verbose_name="تاریخ ویرایش")
-    finished_at = jmodels.jDateTimeField(
-        null=True, blank=True, verbose_name="تاریخ اتمام"
-    )
+    created_at = models.DateTimeField(default=datetime.datetime.now, verbose_name="تاریخ ایجاد")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="تاریخ ویرایش")
+    finished_at = models.DateTimeField(null=True, blank=True, verbose_name="تاریخ اتمام")
     is_finished = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = [
-            "created_at",
-        ]
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ["created_at"]
