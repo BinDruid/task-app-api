4611f50c7197ad1eb3d6a178c6afaab62b9d0386
BinDruid <abharya.dev@gmail.com>
2023-10-25 20:47:04 +0330

    style: reformat urls

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,24 +1,13 @@
 from django.urls import path
+
 from apps.tasks.views import TaskView
 
 urlpatterns = [
-    path(
-        "",
-        (TaskView.as_view({"post": "create"})),
-        name="new_task",
-    ),
-    path(
-        "recent/",
-        (TaskView.as_view({"get": "list"})),
-        name="tasks",
-    ),
+    path("", (TaskView.as_view({"post": "create"})), name="new_task"),
+    path("recent/", (TaskView.as_view({"get": "list"})), name="tasks"),
     path(
         "<uuid:pk>/",
-        (
-            TaskView.as_view(
-                {"get": "retrieve", "delete": "destroy", "patch": "partial_update"}
-            )
-        ),
+        (TaskView.as_view({"get": "retrieve", "delete": "destroy", "patch": "partial_update"})),
         name="single_task",
     ),
 ]
