fc457323b874dc0ce89c41a7b93967ada1069223
BinDruid <abharya.dev@gmail.com>
2023-10-27 13:59:59 +0330

    chore: remove versioning from path

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("master/", admin.site.urls),
-    path("v1/user/", include("apps.accounts.urls", namespace="accounts")),
-    path("v1/task/", include("apps.tasks.urls", namespace="tasks")),
+    path("user/", include("apps.accounts.urls", namespace="accounts")),
+    path("task/", include("apps.tasks.urls", namespace="tasks")),
 ]
 
 if settings.DEBUG:
