d8af615b028dd8ca2479f27474af023b90fc8ad1
BinDruid <abharya.dev@gmail.com>
2023-10-27 10:45:12 +0330

    refactor: move api docs to home view

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,11 @@ from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path("", include("apps.core.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("master/", admin.site.urls),
     path("v1/user/", include("apps.accounts.urls", namespace="accounts")),
     path("v1/task/", include("apps.tasks.urls", namespace="tasks")),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
 
 if settings.DEBUG:
