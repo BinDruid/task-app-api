78a47fc69436e9b5fd997960281aad282657ce45
BinDruid <abharya.dev@gmail.com>
2023-10-27 19:29:42 +0330

    update: add test for task_detail endpoint refactor: use extra assert for yesterday task aggregation

--- apps/tasks/tests.py
+++ apps/tasks/tests.py
@@ -20,23 +20,53 @@ class TestTaskCreation(TestCase):
     def setUp(self):
         self.user = baker.make(User)
         self.today_tasks_count = 10
+        self.yesterday_count = 20
         today = timezone.now()
         yesterday = today + timedelta(days=1)
         baker.make(Task, owner=self.user, created_at=today, _quantity=self.today_tasks_count)
-        baker.make(Task, owner=self.user, created_at=yesterday, _quantity=20)
+        baker.make(Task, owner=self.user, created_at=yesterday, _quantity=self.yesterday_count)
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
-    def test_recent_tasks_aggregation(self):
-        response = self.client.get(RECENT_TASKS_URL)
-        today = date.today().strftime("%d %b, %Y")
+    def test_get_single_task_details(self):
+        task = baker.make(Task, owner=self.user)
+        url = reverse("tasks:detail", kwargs={"pk": task.pk})
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data[today]), self.today_tasks_count)
+        self.assertEqual(response.data["id"], str(task.id))
 
-    def test_user_can_create_task(self):
+    def test_create_single_task(self):
         payload = {"title": "new_test_task", "description": "test task to create"}
         response = self.client.post(TASKS_CREATE_URL, data=payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["title"], payload["title"])
+
+    def test_update_single_task(self):
+        task = baker.make(Task, owner=self.user)
+        url = reverse("tasks:detail", kwargs={"pk": task.pk})
+        payload = {"title": "test_update"}
+        response = self.client.patch(url, data=payload)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], payload["title"])
+
+    def test_delete_single_task(self):
+        task = baker.make(Task, owner=self.user)
+        url = reverse("tasks:detail", kwargs={"pk": task.pk})
+        response = self.client.delete(url)
+        task_exists = Task.objects.filter(pk=task.pk).exists()
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(task_exists)
+
+    def test_recent_tasks_aggregation(self):
+        response = self.client.get(RECENT_TASKS_URL)
+        today = date.today().strftime("%d %b, %Y")
+        yesterday = (date.today()+ timedelta(days=1)).strftime("%d %b, %Y")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data[today]), self.today_tasks_count)
+        self.assertEqual(len(response.data[today]), self.today_tasks_count)
+        self.assertEqual(len(response.data[yesterday]), self.yesterday_count)
