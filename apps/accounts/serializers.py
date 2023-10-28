f2efc646215a6ee3270e8fe9be4651cb6611778d
abharya <abharya.dev@gmail.com>
2023-10-28 11:51:45 +0330

    refactor: use create_user in its serializer

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -12,7 +12,4 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User(email=validated_data["email"], username=validated_data["username"])
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
