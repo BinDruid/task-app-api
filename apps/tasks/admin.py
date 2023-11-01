d19c521bc300bd1edaaa858555200bb07031234f
BinDruid <abharya.dev@gmail.com>
2023-11-01 21:18:22 +0330

    update: add tags to admin panel

--- apps/tasks/admin.py
+++ apps/tasks/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import Tag, Task
 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_per_page = 15
     list_display = ["title", "owner"]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_per_page = 15
+    list_display = ["title", "owner"]
+
