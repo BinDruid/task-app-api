f2bb31c4d8d6ff21d3df7f1d2819f4e695a860de
BinDruid <abharya.dev@gmail.com>
2023-11-08 20:46:50 +0330

    feat: enable celery tasks

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,11 @@
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework.views import APIView
+
+from api.core.tasks import email_task
+
+
+class CeleryTaskView(APIView):
+    def get(self, request):
+        email_task.delay()
+        resp_json = {"message": "calling celery task"}
+        return Response(resp_json)
