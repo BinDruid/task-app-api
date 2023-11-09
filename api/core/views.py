0f6ed3535684e7e18da190d455d053c29318316a
BinDruid <abharya.dev@gmail.com>
2023-11-09 09:10:30 +0330

    chore: add permission to celery task view

--- api/core/views.py
+++ api/core/views.py
@@ -1,11 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 
 from api.core.tasks import email_task
 
 
 class CeleryTaskView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def post(self, request):
         task = email_task.delay()
-        resp_json = {"message": f"calling celery task with id:{task.id}"}
+        resp_json = {"message": f"calling celery task with id: {task.id}"}
         return Response(resp_json)
