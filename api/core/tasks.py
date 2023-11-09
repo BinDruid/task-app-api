161f7ce77abf54ccdf14d561d28365c1a41d7138
BinDruid <abharya.dev@gmail.com>
2023-11-09 15:46:12 +0330

    chore: add logger for celery task

--- api/core/tasks.py
+++ api/core/tasks.py
@@ -1,10 +1,11 @@
 from time import sleep
 from celery import shared_task
+from celery.utils.log import get_task_logger
 
+logger = get_task_logger(__name__)
 
 @shared_task()
 def email_task():
-    print(f"sending a temp email")
+    logger.info("task called")
     sleep(5)
-    print(f"email sent")
     return "done"
